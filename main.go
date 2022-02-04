9aaea0ea0d5c02a2fd9ad9c06d289ecaddbce5de
bismark <bismarkcastilla@gmail.com>
2022-02-04 17:56:41 -0400

    receiver function

--- main.go
+++ main.go
@@ -1,8 +1,13 @@
 package main
 
-import "fmt"
-
 func main() {
-	var card string = "Ace of Spades"
-	fmt.Printf(card)
+	cards := deck{"Ace of Diamonds", newCard()}
+	cards = append(cards, "Six of Spades")
+
+	cards.print()
+
+}
+
+func newCard() string {
+	return "Five of Diamonds"
 }
